cacb9410a049eb2dd46f4952e37cfa013746af99
Magdielradio <magdielradio@gmail.com>
2026-09-23 01:09:04 -0400

    Update server to listen on environment port or 3000

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { WebSocketServer } = require('ws');
 const http = require('http');
 const server = http.createServer();
-server.listen(3000);
+server.listen(process.env.PORT || 3000, '0.0.0.0');
 const wss = new WebSocketServer({server});
